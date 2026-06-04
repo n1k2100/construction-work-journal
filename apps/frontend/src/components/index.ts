ad79ae4ee7806d7d543302f8d027260c84554af6
n1k <n1k@the-dev.ru>
2026-06-04 12:03:47 +0500

    feat: экспортированы компоненты

---
+++ apps/frontend/src/components/index.ts
@@ -0,0 +1,4 @@
+export * from './CrudTableLayout/CrudTableLayout'
+export * from './WorkUpsertModal/WorkUpsertModal'
+export * from './EmployeeUpsertModal/EmployeeUpsertModal'
+export * from './WorkTypeUpsertModal/WorkTypeUpsertModal'
