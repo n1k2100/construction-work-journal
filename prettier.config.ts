f8a8c470d42c0374a81158e7597c6cdf9422eb51
n1k <n1k@the-dev.ru>
2026-06-04 11:42:54 +0500

    chore: добавлена конфигурация prettier

---
+++ prettier.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "prettier";
+
+/**
+ * Конфигурация prettier
+ */
+const config: Config = {
+  semi: false,
+  quoteProps: "consistent",
+  singleQuote: true,
+  jsxSingleQuote: true,
+};
+
+export default config;
