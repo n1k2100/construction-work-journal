fd71a221241112a0b8eb25391fc5769637b6064e
n1k <n1k@the-dev.ru>
2026-06-04 12:06:23 +0500

    feat: реализована страница 404

---
+++ apps/frontend/src/pages/Page404/Page404.tsx
@@ -0,0 +1,19 @@
+import { Button, Result } from 'antd'
+import { Link } from 'react-router-dom'
+
+/**
+ * Неопределенная страница
+ */
+export function Page404() {
+  return (
+    <Result
+      status={'error'}
+      title='Страница не найдена!'
+      extra={
+        <Link to='/'>
+          <Button type='primary'>На главную</Button>
+        </Link>
+      }
+    />
+  )
+}
